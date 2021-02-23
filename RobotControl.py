6bac21f5170e1cd519687a540eecd13fef4e3405
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-02-23 20:36:05 +0000

    Updated servo and camera control into robot control. Made robot.py easier to read. Added comments

--- RobotControl.py
+++ RobotControl.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 from _XiaoRGEEK_SERVO_ import XR_Servo
-from time import sleep
+import time
+import numpy as np
+import cv2
 
-## Set GPIO call mode as BCM
+# Set GPIO call mode as BCM
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 # Initializing Servo for robot arms
 Servo = XR_Servo()
 
-## Define Ports
+# Define Ports
 ENA = 13
 ENB = 20
 IN1 = 19
@@ -18,16 +20,24 @@ IN3 = 21
 IN4 = 26
 
 # Initial Values
-Speed = 75
+Speed = 0
 S1_Angle = 10
 S2_Angle = 120
 S3_Angle = 90
 S4_Angle = 0
+S7_Angle = 0
+S8_Angle = 90
 img_counter = 0
+img = 0
+x = 0
+y = 0
+angle = 0
 Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
 Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
 Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
 Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)
+Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
+Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
 
 # Port Setup
 print("Start")
@@ -43,8 +53,8 @@ RightM = GPIO.PWM(ENA, 1000)
 LeftM = GPIO.PWM(ENB, 1000)
 
 # Start PWM
-RightM.start(75)
-LeftM.start(75)
+RightM.start(Speed)
+LeftM.start(Speed)
 
 # Define movement functions
 def Forward():
@@ -54,17 +64,6 @@ def Forward():
     GPIO.output(IN2, True)
     GPIO.output(IN3, False)
     GPIO.output(IN4, True)
-
-def ForRight():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, True)
-    RightM.ChangeDutyCycle(Speed+25)
-    LeftM.ChangeDutyCycle(Speed-40)
-
 def Backward():
     GPIO.output(ENA, True)
     GPIO.output(ENB, True)
@@ -72,7 +71,6 @@ def Backward():
     GPIO.output(IN2, False)
     GPIO.output(IN3, True)
     GPIO.output(IN4, False)
-
 def Right():
     GPIO.output(ENA, True)
     GPIO.output(ENB, True)
@@ -80,7 +78,6 @@ def Right():
     GPIO.output(IN2, False)
     GPIO.output(IN3, False)
     GPIO.output(IN4, True)
-
 def Left():
     GPIO.output(ENA, True)
     GPIO.output(ENB, True)
@@ -88,7 +85,6 @@ def Left():
     GPIO.output(IN2, True)
     GPIO.output(IN3, True)
     GPIO.output(IN4, False)
-
 ## Define a function to stop
 def Stop():
     GPIO.output(ENA, True)
@@ -99,16 +95,16 @@ def Stop():
     GPIO.output(IN4, False)
 
 class Robot_Control():
+    '''
+    Robot Control with keyboard
+    '''
     def __init__(self, key: str):
+        global S1_Angle, S2_Angle, S3_Angle, S4_Angle, S7_Angle, S8_Angle, Speed
 
+        # Robot Movements with WASD
         if key == 'w':
             Forward()
             print('Moving forward')
-        elif key == 'wd':
-            ForRight()
-            print('Moving north-east')
-        elif key == 'wa':
-            print('Moving north-west')
         elif key == 'd':
             Right()
             print('Turning right')
@@ -118,14 +114,135 @@ class Robot_Control():
         elif key == 's':
             Backward()
             print('Moving backwards')
-        elif key == 'sa':
-            print('Moving south-west')
-        elif key == 'sd':
-            print('Moving south-east')
-
         elif key == 'STOP':
             Stop()
 
+        # Accelerating and Deccelerating with LSHIFT and LCTRL, respectively
+        elif key == 'LSHIFT':
+            if Speed >= 100:
+                Speed = 100
+            else: Speed += 10
+            RightM.ChangeDutyCycle(Speed)
+            LeftM.ChangeDutyCycle(Speed)
+        elif key == 'LCTRL':
+            if Speed <= 0:
+                Speed = 0
+            else: Speed -= 10
+            RightM.ChangeDutyCycle(Speed)
+            LeftM.ChangeDutyCycle(Speed)
 
+        # Servo 1 Control
+        elif key == 'KP4':
+            if S1_Angle <= 0:
+                S1_Angle = 0
+            else:
+                S1_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
+        elif key == 'KP7':
+            if S1_Angle >= 180:
+                S1_Angle = 180
+            else:
+                S1_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
+        # Servo 2 Control
+        elif key == 'KP9':
+            if S2_Angle <= 0:
+                S2_Angle = 0
+            else:
+                S2_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
+        elif key == 'KP6':
+            if S2_Angle >= 180:
+                S2_Angle = 180
+            else:
+                S2_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
+        # Servo 3 Control
+        elif key == 'KP3':
+            if S3_Angle >= 180:
+                S3_Angle = 180
+            else:
+                S3_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
+        elif key == 'KP1':
+            if S3_Angle <= 0:
+                S3_Angle = 0
+            else:
+                S3_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
+        # Servo 4 Control
+        elif key == 'KP_MINUS':
+            if S4_Angle <= 0:
+                S4_Angle = 0
+            else:
+                S4_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)
+        elif key == 'KP_PLUS':
+            if S4_Angle >= 90:
+                S4_Angle = 90
+            else:
+                S4_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)
 
+        # Camera control with arrow keys
+        elif key == 'UP':
+            if S7_Angle <= 0:
+                S7_Angle = 0
+            else:
+                S7_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
+        elif key == 'DOWN':
+            if S7_Angle >= 90:
+                S7_Angle = 90
+            else:
+                S7_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
+        elif key == 'LEFT':
+            if S8_Angle <= 0:
+                S8_Angle = 0
+            else:
+                S8_Angle -= 5
+            Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
+        elif key == 'RIGHT':
+            if S8_Angle >= 90:
+                S8_Angle = 90
+            else:
+                S8_Angle += 5
+            Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
 
+class Robot_Camera():
+    def __init__(self):
+        self.status = True
+        ret, frame = cam.read()
+        cv2.imshow('frame', frame)
+
+        k = cv2.waitKey(1)
+
+        if k % 256 == 27:  # ESC
+            print("Closing...")
+            self.status = False
+            cam.release()
+            cv2.destroyAllWindows()
+        elif k % 256 == 32:  # Space
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!\nPress g to grayscale, rotate and resize image!".format(img_name))
+            img_counter += 1
+        elif k % 256 == ord('g'):  # Change to grayscale
+            # Reading Image
+            img = input("which picture would you like to make grayscale?")
+            gray_image = cv2.imread("opencv_frame_{}.png".format(img), 0)
+            print("Changing opencv_frame_{}.png to grayscale!".format(img))
+            # cv2.imshow("Grayscale Image", gray_image) # Debug
+
+            y = int(input("what height would you want for the image?"))
+            x = int(input("what length would you want for the image?"))
+            print("resizing image to [{},{}]".format(x, y))
+            numpy_image = np.asarray(gray_image)
+            resize_image = cv2.resize(numpy_image, (x, y))
+
+            rotate_image = cv2.rotate(resize_image, cv2.ROTATE_90_CLOCKWISE)
+            cv2.imshow("Edited Image", rotate_image)
+
+        elif k % 256 == ord('x'):  # Close and reset windows
+            cv2.destroyAllWindows()
--- robot.py
+++ robot.py
@@ -1,247 +1,75 @@
 import os, sys
 from KeyPress_Module import KeyPress
-#from RobotControl import Robot_Control
+from RobotControl import Robot_Control, Robot_Camera
 
-import RPi.GPIO as GPIO
-from _XiaoRGEEK_SERVO_ import XR_Servo
-from time import sleep
 
 # Setup pygame
 Kp = KeyPress()
 # https://www.pygame.org/docs/ref/key.html
 
-## Set GPIO call mode as BCM
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-# Initializing Servo for robot arms
-Servo = XR_Servo()
-
-## Define Ports
-ENA = 13
-ENB = 20
-IN1 = 19
-IN2 = 16
-IN3 = 21
-IN4 = 26
-
-# Initial Values
-Speed = 75
-S1_Angle = 10
-S2_Angle = 120
-S3_Angle = 90
-S4_Angle = 0
-S7_Angle = 0
-S8_Angle = 90
-img_counter = 0
-Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
-Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
-Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
-Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)
-Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
-Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
-
-# Port Setup
-print("Start")
-GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ENB, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
-
-# Specify the PWM control port and the frequency of the PWM signal
-RightM = GPIO.PWM(ENA, 1000)
-LeftM = GPIO.PWM(ENB, 1000)
-
-# Start PWM
-RightM.start(75)
-LeftM.start(75)
-
-# Define movement functions
-def Forward():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, True)
-
-def ForLeft():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, True)
-    RightM.ChangeDutyCycle(100)
-    LeftM.ChangeDutyCycle(45)
-
-def ForRight():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, True)
-    RightM.ChangeDutyCycle(45)
-    LeftM.ChangeDutyCycle(100)
-
-def Backward():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, True)
-    GPIO.output(IN2, False)
-    GPIO.output(IN3, True)
-    GPIO.output(IN4, False)
-
-def Left():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, True)
-    GPIO.output(IN2, False)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, True)
-
-def Right():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, True)
-    GPIO.output(IN3, True)
-    GPIO.output(IN4, False)
-
-## Define a function to stop
-def Stop():
-    GPIO.output(ENA, True)
-    GPIO.output(ENB, True)
-    GPIO.output(IN1, False)
-    GPIO.output(IN2, False)
-    GPIO.output(IN3, False)
-    GPIO.output(IN4, False)
-
 def main():
-    global S1_Angle
-    global S2_Angle
-    global S3_Angle
-    global S4_Angle
-    global S7_Angle
-    global S8_Angle
+    '''
+    Main function, sends Keyboard press to robot class
+    '''
     window = True
 
+    # Robot movement control
     if Kp.getKey('w'):
-        if Kp.getKey('d'):
-            ForRight()
-            #Robot_Control('wd')
-        elif Kp.getKey('a'):
-            ForLeft()
-            #Robot_Control('wa')
-        else: Forward() #Robot_Control('w')
+        Robot_Control('w')
     elif Kp.getKey('s'):
-        '''if Kp.getKey('d'):
-            Robot_Control('sd')
-        elif Kp.getKey('a'):
-            Robot_Control('sa')
-        else: Robot_Control('s')'''
-        Backward()
+        Robot_Control('s')
     elif Kp.getKey('a'):
-        Right()
-        #Robot_Control('a')
+        Robot_Control('a')
     elif Kp.getKey('d'):
-        Left()
-        #Robot_Control('d')
-    #elif Kp.getKey('LSHIFT'):
-        #Robot_Control('LSHIFT')
+        Robot_Control('d')
+
+    # Robot acceleration/decceleration
+    elif Kp.getKey('LSHIFT'):
+        Robot_Control('LSHIFT')
+    elif Kp.getKey('LCTRL'):
+        Robot_Control('LCTRL')
+
+    # Robot Arm control, Servos 1-4
+    elif Kp.getKey('KP4'):         # Servo 1
+        Robot_Control('KP4')
+    elif Kp.getKey('KP7'):
+        Robot_Control('KP7')
+    elif Kp.getKey('KP9'):         # Servo 2
+        Robot_Control('KP9')
+    elif Kp.getKey('KP6'):
+        Robot_Control('KP6')
+    elif Kp.getKey('KP3'):         # Servo 3
+        Robot_Control('KP3')
+    elif Kp.getKey('KP1'):
+        Robot_Control('KP1')
+    elif Kp.getKey('KP_MINUS'):    # Servo 4
+        Robot_Control('KP_MINUS')
+    elif Kp.getKey('KP_PLUS'):
+        Robot_Control('KP_PLUS')
+
+    # Camera control, Servos 7 & 8
+    elif Kp.getKey('UP'):          # Servo 7
+        Robot_Control('UP')
+    elif Kp.getKey('DOWN'):
+        Robot_Control('DOWN')
+    elif Kp.getKey('LEFT'):        # Servo 8
+        Robot_Control('LEFT')
+    elif Kp.getKey('RIGHT'):
+        Robot_Control('RIGHT')
+
+    # Quit pygame and stop running code
     elif Kp.getKey('q'):
         print('quitting')
         window = False
 
-    # Servo 1 Control
-    elif Kp.getKey('KP4'):
-        if S1_Angle <= 0:
-            S1_Angle = 0
-        else:
-            S1_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
-    elif Kp.getKey('KP7'):
-        if S1_Angle >= 180:
-            S1_Angle = 180
-        else:
-            S1_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(1, S1_Angle)
-    # Servo 2 Control
-    elif Kp.getKey('KP9'):
-        if S2_Angle <= 0:
-            S2_Angle = 0
-        else:
-            S2_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
-    elif Kp.getKey('KP6'):
-        if S2_Angle >= 180:
-            S2_Angle = 180
-        else:
-            S2_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(2, S2_Angle)
-    # Servo 3 Control
-    elif Kp.getKey('KP3'):
-        if S3_Angle >= 180:
-            S3_Angle = 180
-        else:
-            S3_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
-    elif Kp.getKey('KP1'):
-        if S3_Angle <= 0:
-            S3_Angle = 0
-        else:
-            S3_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(3, S3_Angle)
-    # Servo 4 Control
-    elif Kp.getKey('KP_MINUS'):
-        if S4_Angle <= 0:
-            S4_Angle = 0
-        else:
-            S4_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)9*
-    elif Kp.getKey('KP_PLUS'):
-        if S4_Angle >= 90:
-            S4_Angle = 90
-        else:
-            S4_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(4, S4_Angle)
-
-    # Servo 7 and 8
-    elif Kp.getKey('UP'):
-        if S7_Angle <= 0:
-            S7_Angle = 0
-        else:
-            S7_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
-    elif Kp.getKey('DOWN'):
-        if S7_Angle >= 90:
-            S7_Angle = 90
-        else:
-            S7_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(7, S7_Angle)
-    elif Kp.getKey('LEFT'):
-        if S8_Angle <= 0:
-            S8_Angle = 0
-        else:
-            S8_Angle -= 5
-        Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
-    elif Kp.getKey('RIGHT'):
-        if S8_Angle >= 90:
-            S8_Angle = 90
-        else:
-            S8_Angle += 5
-        Servo.XiaoRGEEK_SetServoAngle(8, S8_Angle)
-
-    else:
-        Stop()
-        #Robot_Control('STOP')
+    # When not pressing key, robot will stop
+    else: Robot_Control('STOP')
     return window
 
 if __name__ == '__main__':
     while True:
         window = main()
-        if window == False:
+        Robot_Camera()
+        camera = Robot_Camera().status
+        if window == False or camera == False:
             break
