aab1089e4c802981d883ee7cbc80254e3c8af526
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-03-10 15:04:14 +0000

    commendted plot module for the moment to test real-time cv2

--- simon_dev/cv2_module.py
+++ simon_dev/cv2_module.py
@@ -2,22 +2,28 @@ import cv2
 import numpy as np
 
 class openCV():
-    def __init__(self):
+    def __init__(self, x, y):
         # Create a black image (size:512*512)
         self.img = np.zeros((512, 512, 3), np.uint8)
         # Draw two digital blue lines
         cv2.line(self.img, (0, 256), (512, 256), (255, 0, 0), 1)
         cv2.line(self.img, (256, 0), (256, 512), (255, 0, 0), 1)
+        # Draw a filled cycle
+        cv2.circle(self.img, (x, y), 28, (0, 255, 255), -1)
         # Show the result
         self.winName = 'Electronic Level'
+        cv2.imshow(self.winName, self.img)
         cv2.namedWindow(self.winName)
 
-    def Update(self, x, y):
-        # Draw a filled cycle
-        cv2.circle(self.img, (x,y), 28, (0, 255, 255), -1)
-        cv2.imshow(self.winName, self.img)
-        cv2.waitKey(500)
-        cv2.destroyAllWindows()
+        k = cv2.waitKey(1) % 256
+        if k == ord('q'):
+            cv2.destroyAllWindows()
+
+    def Destroy(self):
+        pass
+
+
+
 
 
 
--- simon_dev/main.py
+++ simon_dev/main.py
@@ -1,17 +1,17 @@
 import sys
 from mpu_module import mpu
 from cv2_module import openCV
-from plot_module import plot
+#from plot_module import plot
 
 mpu9250 = mpu()
-OpenCV = openCV()
+
 
 def main():
         [x ,y] = mpu9250.read_accel()
-        OpenCV.Update(x, y)
+        openCV(x, y)
 
-        [mx, my, avg_mx, avg_my] = mpu9250.read_mag()
-        plot(mx,my,avg_mx,avg_my)
+        #[mx, my, avg_mx, avg_my] = mpu9250.read_mag()
+        #plot(mx,my,avg_mx,avg_my)
 
 if __name__ == '__main__':
     try:
