8b36549bae77d0eac2b7f03102ad785649710543
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-03-11 12:37:37 +0000

    Edited main files for purpose of only calibrating

--- simon_dev/Calibration/cal_plot.py
+++ simon_dev/Calibration/cal_plot.py
@@ -4,6 +4,7 @@ import numpy as np
 class plot():
     def __init__(self, mx, my, avg_mx, avg_my):
         rec_win = plt.figure(1)
+        rec_win.canvas.set_window_title('Recorded Data')
         # x-axis value
         plt.plot(mx, my, 'o', color='blue')
         # naming the x axis
@@ -15,26 +16,24 @@ class plot():
         # function to show the plot
 
         avg_win = plt.figure(2)
+        avg_win.canvas.set_window_title('Averaged Data')
         plt.plot(avg_mx, avg_my, 'o', color='blue')
         plt.xlabel('mx')
         plt.ylabel('my')
         plt.title('Averaged Data')
 
         # Printing values to normalize values for [-1, 1]
-        print(f"min-mx:{min(avg_mx)}, max-mx:{max(avg_mx)}")
-        print(f"min-my:{min(avg_my)}, max-my:{max(avg_my)}")
+        print(f"min-mx:{min(avg_mx)}\nmax-mx:{max(avg_mx)}")
+        print(f"min-my:{min(avg_my)}\nmax-my:{max(avg_my)}")
 
         normalized_mx = 2 * ((avg_mx - np.min(avg_mx)) / (np.max(avg_mx) - np.min(avg_mx))) - 1
         normalized_my = 2 * ((avg_my - np.min(avg_my)) / (np.max(avg_my) - np.min(avg_my))) - 1
 
-        cal_win = plt.figure(3,figsize=(600,600))
-        plt.fig
+        cal_win = plt.figure(3)
+        cal_win.canvas.set_window_title('Calibrated Data')
         plt.plot(normalized_mx, normalized_my, 'o', color='blue')
         plt.xlabel('mx')
         plt.ylabel('my')
         plt.title('Calibrated Data')
 
         plt.show()
-
-    def calibrate(self):
-        pass
--- simon_dev/task1-mpu.py
+++ simon_dev/task1-mpu.py
@@ -1,15 +1,3 @@
-# coding: utf-8
-## @package faboMPU9250
-#  This is a library for the FaBo 9AXIS I2C Brick.
-#
-#  http://fabo.io/202.html
-#
-#  Released under APACHE LICENSE, VERSION 2.0
-#
-#  http://www.apache.org/licenses/
-#
-#  FaBo <info@fabo.io>
-
 import FaBo9Axis_MPU9250
 import time
 import sys
