bb2d3015a635965ab2a57fd54fa203b583a0b669
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-04-20 00:18:03 +0100

    Added method to count images in data sets

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,5 +1,23 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
+import glob
+from pathlib import Path
 
-print(tf.__version__)
+labels_csv = pd.read_csv('labels.csv')
+data_dir = Path('Datasets/')
+
+images, labels = [], []
+for filename in glob.glob('Datasets/0/*.png'):
+    im = Image.open(filename)
+    data = np.asarray(im)
+    images.append(data)
+
+print(images[0].shape)
+
+image_count = len(list(data_dir.glob('*/*.png')))
+print(image_count)
+
+
