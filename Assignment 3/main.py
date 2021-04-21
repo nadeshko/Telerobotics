40f8ce98302faaa47cdb6ef6954565360d3c199b
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-04-21 19:43:59 +0100

    Added data augmentation and analysis

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,3 +1,5 @@
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
+from tensorflow.keras.layers.experimental import preprocessing
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import Sequential, layers
 from skimage import color
@@ -59,25 +61,22 @@ def plot_value_array(i, predictions_array, true_label):
 
 # Initial Parameters
 num_classes = 9
-batch_size = 12000
+num_images = 11429
 img_height = 32
 img_width = 32
+batch_size = 200
 
 class_names = ['30km/hr','50km/hr','70km/hr', '80km/hr','100km/hr', 'Stop', 'Turn Right',
                'Turn Left', 'Straight']
 
 data_dir = Path('Traffic Data')
 
-#images = np.asarray(list(data_dir.glob('*/*')))
-#labels = list(map(int, (os.listdir(data_dir))))
-#print(images.shape)
-
 # Importing images and labels into dataset
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
-    seed = 123,
+    seed = 124,
     image_size = (img_height, img_width),
-    batch_size = batch_size)
+    batch_size = num_images)
 
 # moving images & labels to n-dimension array
 images, labels = [], []
@@ -85,20 +84,43 @@ for image_batch, labels_batch in dataset:
     images = image_batch.numpy().astype("uint8")
     labels = labels_batch.numpy()
     break
+"""
+datagen = ImageDataGenerator(
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest',
+    channel_shift_range = 10.
+)
+
+img = np.expand_dims(images[0], 0)
+aug_iter = datagen.flow(img)
+augmented_images = [next(aug_iter)[0].astype(np.uint8) for i in range(10)]"""
+
+data_augmentation = tf.keras.Sequential([
+    preprocessing.RandomRotation(0.2),
+    preprocessing.RandomZoom(0.2),
+])
 
 # Convert images to grayscale and normalizing to [0,1]
-images = color.rgb2gray(images)
-images = images / 255.0
-
+images = color.rgb2gray(images) / 255.0
+images = np.expand_dims(images, 3)
+print(images.shape)
 # Splitting data into training(0.8) and test(0.2) data's
 train_img, test_img, train_labels, test_labels = train_test_split(images, labels, test_size = 0.2)
+
+#print(train_img.shape)
 
 ### Building the Sequential Model
 # Setting up layers
 model = Sequential([
     # Dense: output = activation(dot(input, kernel "W matrix") + bias)
-    layers.Flatten(input_shape = (32, 32), name = 'Input'), # Tranforms format of images from 2D -> 1D array
-    layers.Dense(1024, activation = 'relu', name = 'Layer1'), # 1024 nodes
+    data_augmentation,
+    layers.Flatten(input_shape = (32, 32), name = 'Input'),  # Tranforms format of images from 2D->1D array
+    layers.Dense(1024, activation = 'relu', name = 'Layer1'),# 1024 nodes
     layers.Dense(512, activation = 'relu', name = 'Layer2'), # 512 nodes
     layers.Dense(256, activation = 'relu', name = 'Layer3'), # 256 nodes
     layers.Dense(128, activation = 'relu', name = 'Layer4'), # 128 nodes
@@ -112,7 +134,10 @@ model.compile(
 # Training model
 epochs = 100
 #history = model.fit(train_img, train_labels, batch_size = 200, epochs = epochs) #
-history = model.fit(train_img, train_labels, validation_data=(test_img, test_labels), batch_size = 200, epochs = epochs)
+history = model.fit(train_img, train_labels,
+                    validation_data=(test_img, test_labels),
+                    batch_size = batch_size,
+                    epochs = epochs)
 
 # Evaluate accuracy and score
 test_loss, test_acc = model.evaluate(test_img, test_labels, verbose = 2)
@@ -140,12 +165,24 @@ model.summary() # DEBUG
 
 # DEBUGGING MODELS
 plot_graph()
+plt.show()
 
+"""
 # Using trained model to test an image
-img = tf.image.resize(mpimg.imread('stop.jpg'), [32, 32]) # Import and resize
+img = tf.image.resize(img_to_array(load_img('stop.jpg')), (32,32))
+img = np.expand_dims(img, 0)
+print(img.shape)
+predictions_single = probability_model.predict(img)
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(9), class_names, rotation = 45)
+plt.show()
+
+
+img = tf.image.resize(mpimg.imread('stop.jpg'), (32, 32) # Import and resize
 img = (np.expand_dims(color.rgb2gray(img), 0))  # Grayscale and turn to batch
+img = np.expand_dims(img, 0)
 predictions_single = probability_model.predict(img)
 plot_value_array(1, predictions_single[0], test_labels)
 _ = plt.xticks(range(9), class_names, rotation = 45)
 print(np.argmax(predictions_single[0]))
-plt.show()
+plt.show()"""
