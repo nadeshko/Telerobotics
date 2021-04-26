80a73495c623fb4e5a0fff2cb5cf18ca0cdf29e1
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-04-26 20:16:44 +0100

    Added a file to use saved model to predict an image using the computers camera

---
+++ Assignment 3/test.py
@@ -0,0 +1,35 @@
+from keras import models
+from PIL import Image
+import numpy as np
+import cv2
+
+# Load saved model
+model = models.load_model('CNN_model') # input: 1x32x32x3
+video = cv2.VideoCapture(0)
+
+while True:
+    _, frame = video.read()
+
+    # convert captures from into RGB
+    im = Image.fromarray(frame, 'RGB')
+
+    # Resizing into 32x32
+    im = im.resize((128,128))
+    img_array = np.array(im)
+
+    img_array = np.expand_dims(img_array, 0)
+
+    # Predicting image using model
+    prediction = int(model.predict(img_array)[0][0])
+
+    # if prediction is 0, which means img is missing, frame = gray color.
+    if prediction == 0:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Capturing', frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
