04cd52ce77a10ee289c2aa91452b10348c6ceda4
William Lubiantoro <69036994+nadeshko@users.noreply.github.com>
2021-05-04 00:23:06 +0900

    added write to csv file to control robot from prediction results

--- Assignment 3/pred.py
+++ Assignment 3/pred.py
@@ -1,5 +1,6 @@
 from keras import models
 from PIL import Image
+import pandas as pd
 import numpy as np
 import cv2
 
@@ -22,38 +23,42 @@ def get_majority(list_range, default=None):
     # check the majority
     return candidate if list_range.count(candidate) >= len(list_range) // 2 else candidate
 
+file_path = 'robot_control.csv'
+
 class_names = ['30km/hr', '50km/hr', '70km/hr', '80km/hr', '100km/hr', 'Stop', 'Turn Right',
                 'Turn Left', 'Straight']
 desired_size = 32
-frameCounter = 0 # DEBUG
+frameCounter = 0
+timeCounter = 0
 
-# Load saved model
-
+# Load saved model + video
 model = models.load_model('saved_model/CNN_model') # input: 1x32x32x3
-video = cv2.VideoCapture('LowRes2.mp4')
+video = cv2.VideoCapture('test2.mp4')
 
 class_labels = []
-class_list, timestamp = [], []
+class_list, time_stamp = [], []
 
 while True:
     frameCounter += 1
     if video.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter: # Debug
-        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        frameCounter = 0
+        array = np.column_stack((class_list, time_stamp))
+        df = pd.DataFrame(array)
+        df.to_csv(file_path, index=False)
+        break
 
     _, frame = video.read()
 
+    # Get video size and keep aspect ratio
     size = frame.shape
     border = int((size[1]-size[0])/2)
     im = cv2.copyMakeBorder(frame, border, border, 0, 0, cv2.BORDER_CONSTANT)
 
-    # convert captures into RGB
+    # Convert captures into RGB
     im = Image.fromarray(im, 'RGB')
 
-    # Resizing into 32x32
+    # Resizing and reshaping video
     im = im.resize((32, 32))
     img_array = np.array(im)
-
     img_array = np.expand_dims(img_array, 0)
 
     # Predicting image using model
@@ -61,17 +66,21 @@ while True:
     predicted_class = class_names[np.argmax(prediction)]
     confidence = round(100*np.max(prediction), 2)
 
-    class_labels.append(np.argmax(prediction))
-
+    # Show text while video is playing
     font = cv2.FONT_HERSHEY_COMPLEX
     cv2.putText(frame, f'{confidence}% {predicted_class}',
                 (int(size[1]-size[1]*0.4), int(size[0]-size[0]*0.1)),
                 font, 1.5, (255, 0, 0), 2, cv2.LINE_4)
     cv2.imshow('Prediction', frame)
 
-
-    class_list.append(get_majority(class_labels[frameCounter-9:frameCounter]))
-    print(f"majority = {class_list}")
+    class_labels.append(np.argmax(prediction))
+    # Ignoring random and uncertain values
+    class_list.append(get_majority(class_labels[frameCounter-23:frameCounter]))
+    timeCounter += 1
+    if frameCounter > 1:
+        if class_list[frameCounter-1] != class_list[frameCounter-2]:
+           timeCounter = 0
+    time_stamp.append(timeCounter)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
